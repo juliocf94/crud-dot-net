4f45b37f21a954a64f47885ed539b5037983230d
Julio Flores <cesarff.julio@gmail.com>
2026-06-21 16:17:03 -0600

    feat: :art: renombrando archivo
    mod del nombre del controlador par usar plurar en lugar de singular

--- Backend/Controllers/EmployeesController.cs
+++ Backend/Controllers/EmployeesController.cs
@@ -4,12 +4,12 @@ using Microsoft.AspNetCore.Mvc;
 namespace Backend.Controllers;
 
 [ApiController]
-[Route("api/employee")]
-public class EmployeeController : ControllerBase
+[Route("api/employees")]
+public class EmployeesController : ControllerBase
 {
     private readonly AppDbContext _context;
 
-    public EmployeeController(AppDbContext context)
+    public EmployeesController(AppDbContext context)
     {
         _context = context;
     }
