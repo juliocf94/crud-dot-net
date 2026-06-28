43f865a857bf8861baac7ca56cb77cd6733c473f
Julio Flores <cesarff.julio@gmail.com>
2026-06-28 11:27:24 -0600

    docs: :memo: ajustes en la documentación

--- Backend/Data/AppDbContext.cs
+++ Backend/Data/AppDbContext.cs
@@ -5,8 +5,7 @@ namespace Backend.Data;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options)
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
 
