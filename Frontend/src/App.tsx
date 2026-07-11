84d8ae01af85b3c44897f290c944e21daf76f2ab
Julio Flores <cesarff.julio@gmail.com>
2026-07-11 16:08:23 -0600

    docs: :memo: ajustes de la organización del código

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,8 @@
 import { useState } from 'react';
-
 import DataTable from '@components/molecules/DataTable';
-
 import { EMPLOYEE_COLUMNS } from '@constants/columns/employee.columns';
-
 import { useEmployees } from '@hooks/useEmployees';
-
 import { toServerPagination } from '@components/molecules/DataTable/utils/pagination-adapter';
-
 import type { ServerPaginationState } from '@typings/pagination';
 
 function App() {
@@ -19,49 +14,29 @@ function App() {
         });
 
     const {
-
         employees,
-
         pagination: paginationInfo,
-
         loading,
-
     } = useEmployees({
-
         pagination,
-
         filters: {
-
             search: '',
-
         },
-
     });
 
     return (
-
         <DataTable
-
             data={employees}
-
             columns={EMPLOYEE_COLUMNS}
-
             loading={loading}
-
             pagination={paginationInfo}
-
             onPaginationChange={(state) => {
-
                 setPagination(
                     toServerPagination(state),
                 );
-
             }}
-
         />
-
     );
-
 }
 
 export default App;
