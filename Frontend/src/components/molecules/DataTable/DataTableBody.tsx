23605f82488447287f2d745e8f7991deb180edad
Julio Flores <cesarff.julio@gmail.com>
2026-07-11 21:36:36 -0600

    feat: :art: comprendiendo la libreria

--- Frontend/src/components/molecules/DataTable/DataTableBody.tsx
+++ Frontend/src/components/molecules/DataTable/DataTableBody.tsx
@@ -3,21 +3,33 @@ import { useDataTableContext } from './provider/DataTableProvider';
 
 export default function DataTableBody() {
     const { table } = useDataTableContext();
+    console.log(table.getRowModel().rows);
 
     return (
         <tbody>
-            {table.getRowModel().rows.map(row => (
-                <tr key={row.id}>
-                    {row.getVisibleCells().map(cell => (
-                        <td key={cell.id}>
-                            {flexRender(
-                                cell.column.columnDef.cell,
-                                cell.getContext(),
-                            )}
+            {table.getRowModel().rows.length > 0 ?
+                (
+                    table.getRowModel().rows.map(row => (
+                        <tr key={row.id}>
+                            {row.getVisibleCells().map(cell => (
+                                <td key={cell.id}>
+                                    {flexRender(
+                                        cell.column.columnDef.cell,
+                                        cell.getContext(),
+                                    )}
+                                </td>
+                            ))}
+                        </tr>
+                    ))
+                ) : (
+                    /* Empty State Block */
+                    <tr>
+                        <td colSpan={table.getVisibleFlatColumns().length}>
+                            No data available.
                         </td>
-                    ))}
-                </tr>
-            ))}
+                    </tr>
+                )
+            }
         </tbody>
     );
 }
