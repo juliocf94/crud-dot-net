b786c7608b20a28edc7a041c6bfd3e509459007e
Julio Flores <cesarff.julio@gmail.com>
2026-07-15 21:38:21 -0600

    feat: :art: migración nueva
    agregando una columna de código unico

--- Backend/Data/Factories/EmployeeFactory.cs
+++ Backend/Data/Factories/EmployeeFactory.cs
@@ -5,9 +5,11 @@ namespace Backend.Data.Factories;
 
 public static class EmployeeFactory
 {
+    private static long _code = 1000000;
     public static Faker<Employee> Create()
     {
         return new Faker<Employee>()
+            .RuleFor(e => e.CodeEmployee, _ => Interlocked.Increment(ref _code))
             .RuleFor(e => e.NameEmployee, f => f.Name.FirstName())
             .RuleFor(e => e.LastNameEmployee, f => f.Name.LastName())
             .RuleFor(e => e.Birthdate, f => f.Date.Past(35))
--- Backend/Models/Employee.cs
+++ Backend/Models/Employee.cs
@@ -1,11 +1,16 @@
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 
 namespace Backend.Models;
 
+[Index(nameof(CodeEmployee), IsUnique = true)]
 public class Employee
 {
     [Key]
     public int IdEmployee { get; set; }
+
+    [Required]
+    public long CodeEmployee { get; set; }
 
     [Required]
     [MaxLength(80)]
